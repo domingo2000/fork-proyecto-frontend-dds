8fce8a76ab230e17a5e1ce7d7628d3baade22ab3
Jose Miguel Quintana <josemiguelquinta@gmail.com>
2022-04-16 01:30:33 -0400

    feat: implemented bag

---
+++ src/components/Bag/BagProduct.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+interface IProduct {
+  id: number
+  title: string
+  price: number
+  qty: number
+  img: string
+}
+
+function BagProduct({product, changeTotal, deleteProduct}: {product: IProduct, changeTotal: (amount: number) => void, deleteProduct: (id: number) => void}) {
+  const [productQty, setProductQty] = useState(product.qty)
+
+  return (
+    <div className='bag-product'>
+      <div className='bag-product-image'>
+        <img src={product.img} alt={product.title}/>
+      </div>
+      <div className='bag-product-info'>
+        <div className="bag-product-title">
+          <h1>{product.title}</h1>
+        </div>
+        <div className="product-qty">
+          <button onClick={() => {
+              if (productQty - 1 < 1) { return }
+              setProductQty(productQty - 1);
+              changeTotal(-product.price);
+            }
+          }>-</button>
+          <h1>{productQty}</h1>
+          <button onClick={() => {
+              setProductQty(productQty + 1);
+              changeTotal(product.price);
+            }
+          }>+</button>
+        </div>
+        <div className='end'>
+          <h1>${product.price * productQty}.00</h1>
+          <button onClick={() => deleteProduct(product.id)}>Remove</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BagProduct
---
+++ src/views/checkout/Bag.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+import BagProduct from '../../components/Bag/BagProduct'
+
+const initialProducts = [
+  {
+    id: 0,
+    title: 'Airpods (3rd generation)',
+    price: 179.00,
+    qty: 1,
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861342000', 
+  },
+  {
+    id: 1,
+    title: '10.9-inch iPad Air Wi-Fi 64GB - Space Gray',
+    price: 599.00,
+    qty: 1,
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-air-select-wifi-spacegray-202203?wid=470&hei=556&fmt=png-alpha&.v=1645066742664',
+  },
+  {
+    id: 2,
+    title: 'iPhone 13 Pro 128GB Alpine Green',
+    price: 999.00,
+    qty: 1,
+    img: 'https://www.apple.com/v/iphone/home/bf/images/overview/compare/compare_iphone_13_pro__bpn3x8hs692a_large.jpg',
+  }
+
+]
+
+function Bag() {
+  const [total, setTotal] = useState(0);
+  const [products, setProducts] = useState(initialProducts);
+
+  useEffect(() => {
+    let total = 0;
+    products.forEach(product => {
+      total += product.price * product.qty;
+    });
+    setTotal(total);
+  }, [products]);
+
+  const deleteProduct = (id: number) => {
+    setProducts(products.filter(product => product.id !== id));
+  }
+
+  const changeTotal = (amount: number) => {
+    setTotal(total + amount);
+  }
+
+  return (
+    <div className='bag-main'>
+      <div className='bag-title'>
+        <h1>Review your bag.</h1>
+      </div>
+      {products.map((product, index) => (
+        <BagProduct key={index} product={product} changeTotal={changeTotal} deleteProduct={deleteProduct}/>
+      ))}
+      <div className="bag-total">
+        <table>
+          <tbody>
+            <tr>
+              <td className="description">Subtotal</td>
+              <td className='value'>${total}.00</td>
+            </tr>
+            <tr>
+              <td className="description">Shipping</td>
+              <td className='value'>FREE</td>
+            </tr>
+            <tr>
+              <td className="description">Tax (19%)</td>
+              <td className='value'>${Math.round(total * .19 * 100) / 100}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h1 className='total'>Total: ${Math.round(total * 1.19 * 100) / 100}</h1>
+      </div>
+
+      <button className='checkout-button'>Check Out</button>
+    </div>
+  )
+}
+
+export default Bag
