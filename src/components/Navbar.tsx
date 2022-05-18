768d6ddfc578a46de887ca634416b207de968571
Jose Miguel Quintana <josemiguelquinta@gmail.com>
2022-05-18 19:07:05 -0400

    feat: added styles to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,22 +3,31 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className='flex justify-between h-12 bg-[#313132] text-white'>
+      <ul className='flex items-center pl-32'>
+        <li className='mx-2 text-lg hover:ring-2 p-1 rounded-md ring-gray-300'>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className='mx-2 text-lg hover:ring-2 p-1 rounded-md ring-gray-300'>
           <Link to="/products">Products</Link>
         </li>
-        <li>
-          <Link to="/orders">Orders</Link>
+        <li className='mx-2 text-lg hover:ring-2 p-1 rounded-md ring-gray-300'>
+          <Link to="/categories">Categories</Link>
         </li>
-        <li>
+      </ul>
+
+      <ul className='flex items-center pr-32'>
+        <li className='mx-2 text-lg hover:ring-2 p-1 rounded-md ring-gray-300'>
           <Link to="/bag">Bag</Link>
         </li>
-        <li>
-          <Link to="/categories">Categories</Link>
+        <li className='mx-2 text-lg hover:ring-2 p-1 rounded-md ring-gray-300'>
+          <Link to="/orders">My Orders</Link>
+        </li>
+        <li className='mx-2 text-lg hover:ring-2 p-1 rounded-md ring-gray-300'>
+          <Link to="/">Login</Link>
+        </li>
+        <li className='mx-2 text-lg hover:ring-2 p-1 rounded-md ring-gray-300'>
+          <Link to="/">Register</Link>
         </li>
       </ul>
     </nav>
