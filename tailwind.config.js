9041cf797611101006b9154c8c1dd5491474bfcd
Jose Miguel Quintana <josemiguelquinta@gmail.com>
2022-05-18 19:05:06 -0400

    fix: tailwind css instalation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{html, js, ts}"],
+  mode: 'jit',
+  purge: ['src/**/*.{js,jsx,ts,tsx}', 'public/**/*.html'],
+  content: [],
   theme: {
-    extend: {},
+    backgroundImage: {
+      'studio-display': 'url("https://www.apple.com/v/home/an/images/promos/studio-display/promo_studiodisplay_avail__d1cx9j73ooq6_large.jpg")',
+      'all-products': 'url("https://www.apple.com/v/home/an/images/promos/trade-in-may-2022/promo_trade_in_may_2022__bbvnbfzoqzxu_large.jpg")',
+    },
+    extend: {
+      colors: {
+        'home-gray': '#fbfafd',
+      },
+    },
   },
   plugins: [],
 }
