a60f1d2130e571ed4c320619e4aae717264503d9
Jose Miguel Quintana <josemiguelquinta@gmail.com>
2022-05-17 21:53:25 -0400

    feat: implemented categories views

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+import useFetchData from '../services/useFetchData';
+import { CategoryWithProducts } from '../components/Category/Category';
 
 function Home() {
+  const { response, loading, error } = useFetchData('/categories/1');
   return (
-    <h1>EccomerceApp</h1>
-  )
+    <div>
+      <h1>Home</h1>
+      {loading && <p>Loading...</p>}
+      {error && (
+      <p>
+        Error:
+        {' '}
+        {error.message}
+      </p>
+      )}
+      {response && (
+      <div>
+        <CategoryWithProducts category={response.data} />
+      </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
