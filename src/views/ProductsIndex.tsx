2e1bcdd3f887699dd02008039affbc5efb09104b
Jose Miguel Quintana <josemiguelquinta@gmail.com>
2022-05-27 00:19:40 -0400

    refactor: products index fetch implementation to use useFetchData

--- src/views/ProductsIndex.tsx
+++ src/views/ProductsIndex.tsx
@@ -1,14 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 
 import {IProduct} from '../interfaces/IProduct';
-
 import FilterBar from '../components/Filter/FilterBar';
 import {FilterItem} from '../components/Filter/FilterItem/FilterItem';
 import UiSearch from '../components/UiSearch';
 import Product from '../components/Product';
 import {ICategory} from '../interfaces/ICategory';
 import {Link} from 'react-router-dom';
+import useFetchData from '../services/useFetchData';
 
 function setsDifference(setA: Set<string>, setB: Set<string>) {
   const difference = new Set(setA);
@@ -19,27 +18,24 @@ function setsDifference(setA: Set<string>, setB: Set<string>) {
 };
 
 function ProductsIndex() {
+  const categoriesFetch = useFetchData('/categories');
+  const {response, loading, error} = useFetchData('/products');
   const [categories, setCategories] = useState<ICategory[]>([]);
   const [products, setProducts] = useState<IProduct[]>([]);
   const [renderedProducts, setRenderedProducts] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const responseProducts = await axios.get('http://localhost:3001/products');
-        const responseCategories = await axios.get('http://localhost:3001/categories');
-        setProducts(responseProducts.data);
-        setCategories(responseCategories.data);
-        setRenderedProducts(responseProducts.data);
-        setLoading(false);
-      } catch (err) {
-        setError(true);
-      }
-    };
-    fetchData();
-  }, []);
+    if (categoriesFetch.response) {
+      setCategories(categoriesFetch.response.data);
+    }
+  }, [categoriesFetch]);
+
+  useEffect(() => {
+    if (response) {
+      setProducts(response.data);
+      setRenderedProducts(response.data);
+    }
+  }, [response]);
 
   const handleFilterChange = (filters: Set<string>) => {
     const shouldFilter = !(filters.size === 0);
@@ -67,14 +63,14 @@ function ProductsIndex() {
         <div className="product-index__layout__item-left">
           <FilterBar onFiltersChange={handleFilterChange}>
             {categories.map((category) => (
-              <FilterItem key={category.name} />
+              <FilterItem key={category.name} name={category.name} />
             ))}
           </FilterBar>
         </div>
         <div className="product-index__layout__item-right">
           <UiSearch>
             {renderedProducts.map((product) => (
-              <Link key={product.id} to={`/products/${product.id}`}>
+              <Link key={product.id} to={`/products/${product.id}/checkout`}>
                 <Product key={product.id} product={product} />
               </Link>
             ))}
